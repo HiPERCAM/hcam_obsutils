a7879c36d0448ead385da68d037992fa7bc787b7
Stuart Littlefair <s.littlefair@shef.ac.uk>
2025-11-25 14:41:36 +0000

    just gain

--- src/hcam_obsutils/scripts/ucam/qc/gain.py
+++ src/hcam_obsutils/scripts/ucam/qc/gain.py
@@ -62,12 +62,12 @@ def main():
                 ymin=300,
                 ymax=400,
             )
-            _, bias_level, rno = block_stats(bias[nccd][nwin].data)
+            # _, bias_level, rno = block_stats(bias[nccd][nwin].data)
 
             print("CCD{}, WIN{} ({} {})".format(nccd, nwin, speed, binning))
             print("======================================================")
             print("")
-            print("  Bias level:    {:4.0f} ADU".format(bias_level))
-            print("  Read noise:    {:4.1f} e-".format(rno))
+            # print("  Bias level:    {:4.0f} ADU".format(bias_level))
+            # print("  Read noise:    {:4.1f} e-".format(rno))
             print("  Gain:           {:4.1f} e-/ADU".format(g))
             print("")
